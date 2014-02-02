d004803b6dd9e10a369e693db28ba26533375931
Sergei Zinin <zinin@xakep.ru>
2014-02-02 15:41:04 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StreamrushPlatform::Application.config.secret_key_base = '8d7043754b44a5a078d77e8e18b0435b46b866842eab63ccefe9ec0475b8c66581d023ebb43eeb617c61f70a348b54b0099651644bbf64b705a483ccc77a7e91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StreamrushPlatform::Application.config.session_store :cookie_store, key: '_streamrush_platform_session'
