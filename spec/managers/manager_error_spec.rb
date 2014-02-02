a1ae0cbe4ce5b8a1ea244c592999409f7778f5fc
Sergei Zinin <zinin@xakep.ru>
2014-02-02 23:02:02 +0800

    Session manager specs

--- spec/managers/manager_error_spec.rb
+++ spec/managers/manager_error_spec.rb
@@ -5,4 +5,14 @@ describe ManagerError do
 
   its(:messages) { should == {email: 'is invalid', password: 'is not set'} }
   its(:message) { should == 'email and is invalid and password and is not set' }
+
+  context 'only message given' do
+    subject { described_class.new({message: 'you failed'}) }
+    its(:message) { should == 'you failed' }
+  end
+
+  context 'only one message' do
+    subject { described_class.new({login: 'is invalid'}) }
+    its(:message) { should == 'login is invalid' }
+  end
 end
