80dc8cb1f98ce4683179a38d0ed1fd2c973a1e00
Sergei Zinin <zinin@xakep.ru>
2014-03-05 19:02:02 +0800

    AS3 image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
 
+  # @param filename [String]
+  # @return [String]
+  def cloud_image_path(filename)
+    if Rails.env.development? && Rails.application.assets.find_asset(filename)
+      image_path(filename)
+    else
+      "//s3-us-west-1.amazonaws.com/buddy-assets/images/#{filename}"
+    end
+  end
+
   # @return [String]
   def current_profile_path
     profile_path(current_user.object)
