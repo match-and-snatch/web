43927a047301edc9afb545a4243da2962780a8e9
Sergei Zinin <zinin@xakep.ru>
2014-03-08 04:33:42 +0800

    New assets hash

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ BuddyPlatform::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.0.1'
 
   config.assets.use_cdn = true
 
