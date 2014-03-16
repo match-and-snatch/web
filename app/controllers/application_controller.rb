23b26377a81cddf33292d5adaf9b3ca9ad25c292
Sergei Zinin <zinin@xakep.ru>
2014-03-16 16:10:27 +0800

    Refactor application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,11 +29,6 @@ class ApplicationController < ActionController::Base
     session_manager.current_user.authorized? or error(401)
   end
 
-  # @param code [Integer]
-  def error(code)
-    raise HttpCodeError, code
-  end
-
   # @param action [Symbol]
   # @param subject
   # @raise [ArgumentError] if action or subject are not registered
@@ -49,56 +44,75 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user
 
-  # @return [SessionManager]
-  def session_manager
-    @session_manager ||= SessionManager.new(session)
+  # Renders html used in response to JS
+  # @param _action [String] Action name
+  def default_html(_action)
+    render_to_string(action: _action, layout: false, formats: [:html])
   end
 
+  # @param code [Integer]
+  def error(code)
+    raise HttpCodeError, code
+  end
+
+  # Redirects page on response via JS
   # @param url [String] to redirect to
   def json_redirect(url)
     render json: { status: 'redirect', url: url }
   end
 
+  # Notifies client side about failed operation
   # @param response_params [Hash]
   def json_fail(response_params = {})
     render json: { status: 'failed' }.merge(response_params)
   end
 
+  # Renders failed response with errors hash
   # @param errors [Hash]
   def json_render_errors(errors)
     json_fail errors
   end
 
+  # Notifies client side of successful action
   # @param response_params [Hash]
   def json_success(response_params = {})
     render json: {status: 'success'}.merge(response_params)
   end
 
+  # Renders html with success status
   # @param _action [String]
   def json_render(_action = action_name, opts = {})
-    json = {html: render_to_string(action: _action, layout: false, formats: [:html])}
-    json_success json.merge(opts)
+    json_success({html: default_html(_action)}.merge(opts))
   end
 
+  # Appends html to container
   # @param _action [String]
   def json_append(_action = action_name, opts = {})
-    json = {status: 'append', html: render_to_string(action: _action, layout: false, formats: [:html])}
+    json = {status: 'append', html: default_html(_action)}
     render json: json.merge(opts)
   end
 
+  # Prepends html to container
   # @param _action [String]
   def json_prepend(_action = action_name, opts = {})
-    json = {status: 'prepend', html: render_to_string(action: _action, layout: false, formats: [:html])}
+    json = {status: 'prepend', html: default_html(_action)}
     render json: json.merge(opts)
   end
 
+  # Replaces container with responded html
   # @param _action [String]
   def json_replace(_action = action_name, opts = {})
-    json = {status: 'replace', html: render_to_string(action: _action, layout: false, formats: [:html])}
+    json = {status: 'replace', html: default_html(_action)}
     render json: json.merge(opts)
   end
 
+  # Reloads page via JS
   def json_reload
     render json: {status: 'reload'}
+  end
+
+  # @return [SessionManager]
+  def session_manager
+    @session_manager ||= SessionManager.new(session)
   end
 end
