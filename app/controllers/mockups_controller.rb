4a029ea87ef9b632f5c26dd9d8f47906b511917f
Sergei Zinin <zinin@xakep.ru>
2014-03-27 03:57:59 +0800

    Mocks for account info

--- app/controllers/mockups_controller.rb
+++ app/controllers/mockups_controller.rb
@@ -1,6 +1,14 @@
 class MockupsController < ApplicationController
 
   def show
-    render "mockups/#{params[:mockup]}"
+    respond_to do |format|
+      format.html do
+        render "mockups/#{params[:mockup]}"
+      end
+
+      format.json do
+        json_render template: params[:mockup]
+      end
+    end
   end
 end
