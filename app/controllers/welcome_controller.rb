cca10817e912d7a30c4d2ba87fe5c207ba39530d
Sergei Zinin <zinin@xakep.ru>
2014-03-27 23:48:21 +0800

    Correct redirects

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
   before_filter do
-    redirect_to profile_path(current_user.object) if current_user.authorized?
+    if current_user.authorized?
+      if current_user.has_complete_profile?
+        redirect_to profile_path(current_user.object)
+      else
+        redirect_to account_info_path
+      end
+    end
   end
 
   def show
