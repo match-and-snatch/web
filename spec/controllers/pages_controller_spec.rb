73e2133f05f064f27a1eae9f7c704f1e1b110ba1
Sergei Zinin <zinin@xakep.ru>
2014-04-12 17:20:22 +0800

    Static pages

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe PagesController do
+  describe 'GET #about' do
+    subject { get 'about' }
+    it { should be_success }
+  end
+
+  describe 'GET #pricing' do
+    subject { get 'pricing' }
+    it { should be_success }
+  end
+
+  describe 'GET #contact_us' do
+    subject { get 'contact_us' }
+    it { should be_success }
+  end
+
+  describe 'GET #terms_of_use' do
+    subject { get 'terms_of_use' }
+    it { should be_success }
+  end
+
+  describe 'GET #privacy_policy' do
+    subject { get 'privacy_policy' }
+    it { should be_success }
+  end
+end
