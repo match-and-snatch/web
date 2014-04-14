80886e4bf49fa5723432c995800b9975c86fc710
Sergei Zinin <zinin@xakep.ru>
2014-04-14 21:28:47 +0800

    Basic uploads functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,8 +11,7 @@ class PhotosController < UploadsController
 
   def create
     manager.create_pending_photo(params[:transloadit])
-    json_replace html: render_to_string(partial: 'pending_post_upload',
-                                        collection: current_user.pending_post_uploads(true))
+    json_replace html: render_to_string(partial: 'photo_posts/pending_uploads')
   end
 
   def destroy
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,8 +2,7 @@ class VideosController < UploadsController
 
   def create
     manager.create_pending_video(params[:transloadit])
-    json_replace html: render_to_string(partial: 'pending_post_upload',
-                                        collection: current_user.pending_post_uploads(true))
+    json_replace html: render_to_string(partial: 'video_posts/pending_uploads')
   end
 
   def destroy
