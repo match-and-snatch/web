3a6980d19c648797398bd366962a35ca29044148
Sergei Zinin <zinin@xakep.ru>
2014-04-15 22:36:28 +0800

    Redirect from second step

--- app/controllers/owner/second_steps_controller.rb
+++ app/controllers/owner/second_steps_controller.rb
@@ -7,6 +7,6 @@ class Owner::SecondStepsController < Owner::BaseController
   def update
     UserProfileManager.new(@user).update subscription_cost: params[:subscription_cost],
                                          profile_name:      params[:profile_name]
-    redirect_to third_step_path
+    json_redirect profile_path(@user.reload.slug)
   end
 end
