f1e1c8277d8c10c6134e3c7cc1dfc4a937bf4b8f
Sergei Zinin <zinin@xakep.ru>
2014-04-15 18:20:57 +0800

    Allow user to upload multiple photos

--- app/managers/upload_manager.rb
+++ app/managers/upload_manager.rb
@@ -13,24 +13,38 @@ class UploadManager < BaseManager
     transloadit_data['uploads'][0]                    or fail_with! 'No uploads'
     transloadit_data['uploads'][0]['type'] == 'video' or fail_with! 'Uploaded file is not a video'
 
-    if user.pending_post_uploads.videos.any?
+    if VideoPost.pending_uploads_for(user).any?
       fail_with! "You can't upload more than one video."
     end
 
     thumb = transloadit_data['results']['thumbs'].try(:first) or fail_with! 'No thumb received'
     create(transloadit_data, attributes: { uploadable_type: 'Post',
                                            uploadable_id: nil,
-                                           preview_url: thumb['url']})
+                                           preview_url: thumb['url'] })
   end
 
   # @param transloadit_data [Hash]
-  # @return [Upload]
-  def create_pending_photo(transloadit_data)
-    orignal = transloadit_data["results"][":original"].try(:first) or fail_with! 'No photo received'
-    create(transloadit_data, attributes: { uploadable_type: 'Post',
-                                           uploadable_id: nil,
-                                           preview_url: orignal["url"]})
+  # @return [Array<Upload>]
+  def create_pending_photos(transloadit_data)
+    attributes = { uploadable_type: 'Post', uploadable_id: nil }
+
+    transloadit_data['uploads'].each_with_index.map do |upload_data, index|
+      original = transloadit_data['results'][':original'][index]
+      upload = Upload.new transloadit_data: transloadit_data,
+                          user_id:          user.id,
+                          type:             upload_data['type'],
+                          duration:         upload_data['meta']['duration'],
+                          mime_type:        upload_data['mime'],
+                          width:            upload_data['meta']['width'],
+                          height:           upload_data['meta']['height'],
+                          url:              original['url']
+      upload.attributes = attributes.merge(preview_url: original['url'])
+      upload.save or fail_with! upload.errors
+      upload
+    end
   end
+
+  private
 
   # @param transloadit_data [Hash]
   # @param uploadable [ActiveRecord::Base]
