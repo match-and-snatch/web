b07627ba51f995b2dc6a919b68b7fdae32e205b7
Sergei Zinin <zinin@xakep.ru>
2014-04-22 02:48:32 +0800

    Fixes redirects

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   before_filter do
     if current_user.authorized?
-      if current_user.complete_profile?
+      if current_user.has_profile_page?
         redirect_to profile_path(current_user.object)
       else
         redirect_to account_info_path
