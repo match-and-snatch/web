b99de639fa77b3bc217400b6f7f1356d31229999
Sergei Zinin <zinin@xakep.ru>
2014-04-21 14:50:02 +0800

    Fixes CC validation for AMEX

--- app/managers/concerns/credit_card_validator.rb
+++ app/managers/concerns/credit_card_validator.rb
@@ -2,7 +2,7 @@ module Concerns::CreditCardValidator
 
   # @param card [CreditCard]
   def validate_cc(card)
-    fail_with :number      if card.number.blank? || card.number.length < 16
+    fail_with :number      if card.number.blank? || card.number.length < 14
     fail_with :cvc         if card.cvc.blank?    || card.cvc.length    < 3
     fail_with :expiry_date if card.expiry_month.blank? || card.expiry_year.blank? || card.expiry_month.to_i > 12 || card.expiry_month.to_i < 1 || card.expiry_year.to_i < 14
   end
