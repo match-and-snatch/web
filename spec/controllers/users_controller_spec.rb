a207c116ce58dd234c9b0bd5a0f163401bc3f6d6
Sergey Kutsko <sergey.kutsko@toptal.com>
2014-04-25 14:49:33 +0300

    specs for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -44,14 +44,84 @@ describe UsersController do
     end
   end
 
+  let(:profile) { create_profile email: 'profile@gmail.com' }
   describe 'PUT #update_name' do
-    let(:owner) { create_profile email: 'owner@gmail.com' }
-    subject { put 'update_name', id: owner.slug, profile_name: 'anotherName'}
+    subject { put 'update_name', id: profile.slug, profile_name: 'anotherName'}
 
     context 'authorized access' do
-      before { sign_in owner }
+      before { sign_in profile }
       its(:status){ should == 200 }
       its(:body){ should match_regex /success/ }
+    end
+
+    context 'unauthorized access' do
+      its(:status) { should == 401 }
+    end
+  end
+
+  describe 'PUT #update_cost' do
+    subject { put 'update_cost', id: "anything", cost: 10}
+
+    context 'authorized access' do
+      before { sign_in profile }
+      its(:status){ should == 200 }
+      its(:body){ should match_regex /reload/ }
+    end
+
+    context 'unauthorized access' do
+      its(:status) { should == 401 }
+    end
+  end
+
+  describe 'PUT #update_contacts_info' do
+    subject { put 'update_contacts_info', contacts_info_data_params }
+
+    context 'authorized access' do
+      before { sign_in profile }
+      its(:status){ should == 200 }
+      its(:body){ should match_regex /replace/ }
+    end
+
+    context 'unauthorized access' do
+      its(:status) { should == 401 }
+    end
+  end
+
+  describe 'PUT #update_cover_picture_position' do
+    subject { put 'update_cover_picture_position', id: 'anything', cover_picture_possition: 100 }
+
+    context 'authorized access' do
+      before { sign_in profile }
+      its(:status){ should == 200 }
+      its(:body){ should match_regex /success/ }
+    end
+
+    context 'unauthorized access' do
+      its(:status) { should == 401 }
+    end
+  end
+
+  describe 'PUT #update_cover_picture' do
+    subject { put 'update_cover_picture', cover_picture_data_params }
+
+    context 'authorized access' do
+      before { sign_in profile }
+      its(:status){ should == 200 }
+      its(:body){ should match_regex /replace/ }
+    end
+
+    context 'unauthorized access' do
+      its(:status) { should == 401 }
+    end
+  end
+
+  describe 'PUT #update_profile_picture' do
+    subject { put 'update_profile_picture', profile_picture_data_params }
+
+    context 'authorized access' do
+      before { sign_in profile }
+      its(:status){ should == 200 }
+      its(:body){ should match_regex /replace/ }
     end
 
     context 'unauthorized access' do
