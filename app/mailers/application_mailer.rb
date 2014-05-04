c1dfa8c4a2cb47bf1ef829365b89f2bee7a1afd9
Sergei Zinin <zinin@xakep.ru>
2014-05-04 20:33:09 +0800

    Disable quotation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,14 @@ class ApplicationMailer < ActionMailer::Base
   layout 'mail'
   default from: 'Connectpal <noreply@connectpal.com>'
   default content_type: 'text/html'
+
+  def mail(*args)
+    super(*args) do |format|
+      format.html do
+        Slim::Engine.with_options(pretty: true, tabsize: 1) do
+          render layout: true
+        end
+      end
+    end
+  end
 end
