f04a5581300e8bf4c5593504403201829e35e5b2
Sergei Zinin <zinin@xakep.ru>
2014-05-04 20:50:28 +0800

    Apply multipart alternative emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   layout 'mail'
   default from: 'Connectpal <noreply@connectpal.com>'
-  default content_type: 'text/html'
+  default content_type: 'multipart/alternative'
 
   def mail(*args)
     super(*args) do |format|
@@ -9,6 +9,12 @@ class ApplicationMailer < ActionMailer::Base
         Slim::Engine.with_options(pretty: true, tabsize: 1) do
           render layout: true
         end
+      end
+
+      format.text do
+        render text: (Slim::Engine.with_options(pretty: true, tabsize: 1) do
+          render layout: true, formats: [:html]
+        end)
       end
     end
   end
