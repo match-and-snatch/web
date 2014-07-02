dc873f0cff968c227a821e7cf6105692b43f452d
Sergei Zinin <zinin@xakep.ru>
2014-07-02 21:04:24 +0800

    Fixes empty subscription query

--- app/controllers/admin/profile_owners_controller.rb
+++ app/controllers/admin/profile_owners_controller.rb
@@ -40,7 +40,7 @@ class Admin::ProfileOwnersController < Admin::BaseController
   def total_unsubscribed
     date = Date.parse(params[:date])
     period = date.beginning_of_month..date
-    @subscriptions = Subscription.where(target_user_id: @user.id, removed_at: period, removed: true).map { |s| SubscriptionDecorator.new(s, date) }
+    @subscriptions = Subscription.where(target_user_id: @user.id, removed_at: period, removed: true).where.not(user_id: nil).map { |s| SubscriptionDecorator.new(s, date) }
     json_success popup: render_to_string(action: action_name, layout: false)
   end
 
