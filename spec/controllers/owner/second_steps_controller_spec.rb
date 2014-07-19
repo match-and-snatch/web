c0cab95097b6be8fa7c8eee45fb4d21189c7ecab
Sergei Zinin <zinin@xakep.ru>
2014-07-20 00:18:03 +0800

    Second step specs

---
+++ spec/controllers/owner/second_steps_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe Owner::SecondStepsController, type: :controller do
+  let(:user) { create_user }
+
+  describe 'GET #show' do
+    subject(:perform_request) { get 'show' }
+
+    context 'authorized' do
+      before { sign_in user }
+      it { should be_success }
+
+      context 'already have profile created' do
+        pending 'redirects me to my profile page'
+      end
+    end
+
+    context 'unauthorized' do
+      its(:status) { should == 401 }
+    end
+  end
+
+  describe 'PUT #update' do
+    subject(:perform_request) { put 'update', cost: 10,
+                                              profile_name: 'test',
+                                              holder_name: 'test',
+                                              routing_number: '111111111',
+                                              account_number: '123456' }
+    context 'authorized' do
+      before { sign_in user }
+      it { should be_success }
+
+      context 'already have profile created' do
+        pending 'shows error'
+      end
+    end
+
+    context 'unauthorized' do
+      its(:status) { should == 401 }
+    end
+  end
+end
