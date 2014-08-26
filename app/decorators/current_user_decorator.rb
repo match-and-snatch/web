7030aa9a68c8f9276fa8285241583115a4651888
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2014-08-26 09:57:58 +0400

    show cancelled subscriptions in header menu

--- app/decorators/current_user_decorator.rb
+++ app/decorators/current_user_decorator.rb
@@ -60,7 +60,7 @@ class CurrentUserDecorator < UserDecorator
 
   def idols
     User.joins(source_subscriptions: :target_user).
-      where(subscriptions: {user_id: object.id, removed: false}).
+      where(subscriptions: {user_id: object.id}).
       order('subscriptions.created_at DESC').
       limit(10).map do |user|
       ProfileDecorator.new(user)
