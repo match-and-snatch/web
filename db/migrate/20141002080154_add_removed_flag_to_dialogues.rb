e672726aabcf24c73eb612b30dbeda0f1f08c2cf
Sergei Zinin <zinin@xakep.ru>
2014-10-07 21:40:08 +0800

    Remove migrations

--- db/migrate/20141002080154_add_removed_flag_to_dialogues.rb
+++
@@ -1,6 +0,0 @@
-class AddRemovedFlagToDialogues < ActiveRecord::Migration
-  def change
-    add_column :dialogues, :removed, :boolean, default: false
-    add_column :dialogues, :removed_at, :datetime
-  end
-end
