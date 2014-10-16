e2c926f90de28b09a3c3a8916cfe747bf8816575
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2014-10-16 18:00:57 +0800

    fix dashboard

--- app/presenters/overview_presenter.rb
+++ app/presenters/overview_presenter.rb
@@ -1,71 +1,87 @@
 class OverviewPresenter
 
-  def total_subscribers_count
-    Subscription.count
-  end
-
-  def total_active_subscribers_count
+  def current_subscribers_count
     Subscription.where(removed: false).count
   end
 
-  def total_unsubscribers_count
+  def total_subscribers_count
+    Event.where(action: 'subscription_created').count
+  end
+
+  def current_unsubscribers_count
     Subscription.where(removed: true).count
   end
 
-  def total_failed_payments_count
+  def total_unsubscribers_count
+    Event.where(action: 'subscription_canceled').count
+  end
+
+  def current_failed_payments_count
     User.where(billing_failed: true).count
   end
 
+  def total_failed_payments_count
+    Event.where(action: 'payment_failed').count
+  end
+
   def total_gross_sales
-    Payment.sum(:amount)
+    @total_gross_sales ||= Payment.sum(:amount)
   end
 
   def total_connectpal_fees
-    Payment.sum(:subscription_fees) - self.stripe_fees
+    Payment.sum(:subscription_fees) - self.total_stripe_fees
   end
 
   def total_tos_fees
     Subscription.where(removed: true).sum(:cost)
   end
 
-  def stripe_fees
-    Payment.count * 30 + Payment.sum(:amount) * 0.029
-  end
-
-  def daily_payments
-    Payment.where(created_at: current_day)
+  def total_stripe_fees
+    @total_stripe_fees ||= Payment.count * 30 + self.total_gross_sales * 0.029
   end
 
   def daily_gross_sales
-    self.daily_payments.sum(:amount)
-  end
-
-  def daily_subscribers_count
-    Subscription.where(created_at: current_day).count
-  end
-
-  def daily_unsubscribers
-    Subscription.where(removed_at: current_day)
-  end
-
-  def daily_unsubscribers_count
-    self.daily_unsubscribers.count
-  end
-
-  def daily_failed_payments_count
-    User.joins(:payment_failures).where(payment_failures: { created_at: current_day }).select('users.id').uniq.count
+    @daily_gross_sales ||= daily_payments.sum(:amount)
   end
 
   def daily_tos_fees
-    self.daily_unsubscribers.sum(:cost)
+    daily_unsubscribers.sum(:cost)
   end
 
   def daily_stripe_fees
-    self.daily_payments.count * 30 + daily_gross_sales * 0.029
+    daily_payments.count * 30 + daily_gross_sales * 0.029
   end
-  
+
+  def daily_subscribers_count
+    Subscription.where(removed: false, created_at: current_day).count
+  end
+
+  def daily_total_subscribers_count
+    Event.where(action: 'subscription_created', created_at: current_day).count
+  end
+
+  def daily_unsubscribers_count
+    daily_unsubscribers.count
+  end
+
+  def daily_total_unsubscribers_count
+    Event.where(action: 'subscription_canceled', created_at: current_day).count
+  end
+
+  def daily_failed_payments_count
+    Event.where(action: 'payment_failed', created_at: current_day).count
+  end
+
   private
-  
+
+  def daily_payments
+    @daily_payments ||= Payment.where(created_at: current_day)
+  end
+
+  def daily_unsubscribers
+    @daily_unsubscribers ||= Subscription.where(removed_at: current_day)
+  end
+
   def current_day
     Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
   end
--- spec/presenters/overview_presenter_spec.rb
+++ spec/presenters/overview_presenter_spec.rb
@@ -1,131 +1,156 @@
 require 'spec_helper'
 
 describe OverviewPresenter do
+  subject { described_class.new }
+
   let(:user) { create_user }
   let(:target_user) { create_profile(email: 'target@user.com') }
   let(:subscription) { SubscriptionManager.new(subscriber: user).subscribe_and_pay_for(target_user) }
+  let(:restore) { SubscriptionManager.new(subscription: subscription).restore }
+  let(:unsubscribe) { SubscriptionManager.new(subscription: subscription).unsubscribe }
 
-  subject { described_class.new }
 
   before { StripeMock.start }
   after { StripeMock.stop }
 
   before do
-    UserProfileManager.new(user).update_cc_data(number: '4242424242424242', cvc: '333', expiry_month: '12', expiry_year: 2018)
+    UserProfileManager.new(user).update_cc_data(number: '4242_4242_4242_4242', cvc: '333', expiry_month: '12', expiry_year: 2018)
     subscription
   end
 
-  describe '#total_subscribers_count' do
-    specify do
-      expect(subject.total_subscribers_count).to eq(1)
+  context 'subscribers count' do
+    describe '#current_subscribers_count' do
+      specify { expect(subject.current_subscribers_count).to eq(1) }
+
+      context 'canceled subscription' do
+        specify { expect{ unsubscribe }.to change { subject.current_subscribers_count }.from(1).to(0) }
+      end
+    end
+
+    describe '#total_subscribers_count' do
+      specify { expect(subject.total_subscribers_count).to eq(1) }
+
+      context 'canceled subscription' do
+        specify { expect{ unsubscribe }.not_to change { subject.total_subscribers_count } }
+      end
+    end
+
+    describe '#daily_subscribers_count' do
+      specify { expect(subject.daily_subscribers_count).to eq(1) }
+
+      context 'canceled subscription' do
+        specify { expect{ unsubscribe }.to change { subject.daily_subscribers_count }.from(1).to(0) }
+      end
+    end
+
+    describe '#daily_total_subscribers_count' do
+      specify { expect(subject.daily_total_subscribers_count).to eq(1) }
+
+      context 'canceled subscription' do
+        specify { expect{ unsubscribe }.not_to change { subject.daily_total_subscribers_count } }
+      end
     end
   end
 
-  describe '#total_active_subscribers_count' do
-    specify do
-      expect(subject.total_active_subscribers_count).to eq(1)
+  context 'unsubscribers count' do
+    before { unsubscribe }
+
+    describe '#current_unsubscribers_count' do
+      specify { expect(subject.current_unsubscribers_count).to eq(1) }
+
+      context 'restored subscription' do
+        specify { expect{ restore }.to change { subject.current_unsubscribers_count }.from(1).to(0) }
+      end
+    end
+
+    describe '#total_unsubscribers_count' do
+      specify { expect(subject.total_unsubscribers_count).to eq(1) }
+
+      context 'restored subscription' do
+        specify { expect{ restore }.not_to change { subject.total_unsubscribers_count } }
+      end
+    end
+
+    describe '#daily_unsubscribers_count' do
+      specify { expect(subject.daily_unsubscribers_count).to eq(1) }
+
+      context 'restored subscription' do
+        specify { expect{ restore }.to change { subject.daily_unsubscribers_count }.from(1).to(0) }
+      end
+    end
+
+    describe '#daily_total_unsubscribers_count' do
+      specify { expect(subject.daily_total_unsubscribers_count).to eq(1) }
+
+      context 'restored subscription' do
+        specify { expect{ restore }.not_to change { subject.daily_total_unsubscribers_count } }
+      end
     end
   end
 
-  describe '#total_unsubscribers_count' do
-    before do
-      SubscriptionManager.new(subscription: subscription).unsubscribe
-    end
-
-    specify do
-      expect(subject.total_unsubscribers_count).to eq(1)
-    end
-  end
-
-  context 'failed payment' do
+  context 'failed payments count' do
     before do
       StripeMock.prepare_card_error(:card_declined)
       UserProfileManager.new(user).update_cc_data(number: '4000_0000_0000_0341', cvc: '333', expiry_month: '12', expiry_year: 2018)
       SubscriptionManager.new(subscriber: user).subscribe_and_pay_for(create_profile(email: 'another_target@user.com'))
     end
 
-    describe '#total_failed_payments_count' do
-      specify do
-        expect(subject.total_failed_payments_count).to eq(1)
+    describe '#current_failed_payments_count' do
+      specify { expect(subject.current_failed_payments_count).to eq(1) }
+
+      context 'restored subscription' do
+        let(:restore) { UserProfileManager.new(user).update_cc_data(number: '4242_4242_4242_4242', cvc: '333', expiry_month: '12', expiry_year: 2018) }
+
+        specify { expect { restore }.to change { subject.current_failed_payments_count }.from(1).to(0) }
       end
+    end
+
+    describe '#total_failed_payments_count' do
+      specify { expect(subject.total_failed_payments_count).to eq(1) }
     end
   
     describe '#daily_failed_payments_count' do
-      specify do
-        expect(subject.daily_failed_payments_count).to eq(1)
-      end
+      specify { expect(subject.daily_failed_payments_count).to eq(1) }
     end
   end
 
   describe '#total_gross_sales' do
-    specify do
-      expect(subject.total_gross_sales).to eq(599)
-    end
+    specify { expect(subject.total_gross_sales).to eq(599) }
   end
 
   describe '#total_connectpal_fees' do
-    specify do
-      expect(subject.total_connectpal_fees).to eq(51.629)
-    end
+    specify { expect(subject.total_connectpal_fees).to eq(51.629) }
   end
 
-  describe '#stripe_fees' do
-    specify do
-      expect(subject.stripe_fees).to eq(47.371)
-    end
-  end
-
-  describe '#daily_payments' do
-    specify do
-      expect(subject.daily_payments).to eq([Payment.first])
-    end
+  describe '#total_stripe_fees' do
+    specify { expect(subject.total_stripe_fees).to eq(47.371) }
   end
 
   describe '#daily_gross_sales' do
-    specify do
-      expect(subject.daily_gross_sales).to eq(599)
-    end
+    specify { expect(subject.daily_gross_sales).to eq(599) }
   end
 
-  describe '#daily_subscribers_count' do
-    specify do
-      expect(subject.daily_subscribers_count).to eq(1)
-    end
-  end
-
-  context 'canceled subscriptions' do
-    before do
-      SubscriptionManager.new(subscription: subscription).unsubscribe
-    end
-
-    describe '#daily_unsubscribers' do
-      specify do
-        expect(subject.daily_unsubscribers).to eq([subscription])
-      end
-    end
-
-    describe '#daily_unsubscribers_count' do
-      specify do
-        expect(subject.daily_unsubscribers_count).to eq(1)
-      end
-    end
-
+  context 'tos fees' do
     describe '#daily_tos_fees' do
-      specify do
-        expect(subject.daily_tos_fees).to eq(500)
+      specify { expect { unsubscribe }.to change { subject.daily_tos_fees }.from(0).to(500) }
+
+      context 'restored subscription' do
+        before { unsubscribe }
+        specify { expect{ restore }.to change { subject.daily_tos_fees }.from(500).to(0) }
       end
     end
 
     describe '#total_tos_fees' do
-      specify do
-        expect(subject.total_tos_fees).to eq(500)
+      specify { expect { unsubscribe }.to change { subject.total_tos_fees }.from(0).to(500) }
+
+      context 'restored subscription' do
+        before { unsubscribe }
+        specify { expect{ restore }.to change { subject.total_tos_fees }.from(500).to(0) }
       end
     end
   end
 
   describe '#daily_stripe_fees' do
-    specify do
-      expect(subject.daily_stripe_fees).to eq(47.371)
-    end
+    specify { expect(subject.daily_stripe_fees).to eq(47.371) }
   end
 end
