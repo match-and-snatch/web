c448d500e708b9ba94ae8fa8d9041d0e0d8ce877
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2014-10-23 00:24:57 +0800

    prevent sign_it twice in recent profiles controller spec

--- spec/controllers/admin/recent_profiles_controller_spec.rb
+++ spec/controllers/admin/recent_profiles_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
 describe Admin::RecentProfilesController, type: :controller do
-  before { sign_in create_admin(email: 'admin@gmail.com') }
-
   describe 'GET #index' do
     subject { get 'index' }
-    it { should be_success }
+
+    context 'as admin' do
+      before { sign_in create_admin(email: 'admin@gmail.com') }
+      it { should be_success }
+    end
 
     context 'as a non admin' do
       before { sign_in create_user }
