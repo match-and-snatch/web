8b771bd1e8ff1163376b6dec0e46c08486082af8
Sergei Zinin <zinin@xakep.ru>
2014-10-28 11:36:24 +0700

    Fixes forking on production

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,3 +6,13 @@ else
                            port: 12790,
                            password: 'jJKJDmjSyaHEkBsL')
 end
+
+Resque.before_fork do
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+Resque.after_fork do
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
