086daef50ff80856e271caff617f1b5356f11fac
Sergei Zinin <zinin@xakep.ru>
2015-01-02 23:11:33 +0700

    Default layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def layout
+    @layout ||= {}
+  end
+
+  helper_method :layout
 end
