101382c6f8c19255157c36f594ee489beaa2829f
Sergei Zinin <zinin@xakep.ru>
2015-02-17 21:47:41 +0700

    Move code from controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,30 +1,10 @@
 class VideosController < UploadsController
 
   def playlist
-    if @upload.high_quality_playlist_url
-      hq_playlist = <<-HQ_PLAYLIST
-#EXT-X-STREAM-INF:PROGRAM-ID=1,NAME=High,BANDWIDTH=1677946,RESOLUTION=1920x1080
-#{@upload.high_quality_playlist_url.sub('https:', 'http:')}
-HQ_PLAYLIST
-    end
-
-    if @upload.low_quality_playlist_url
-      lq_playlist = <<-LQ_PLAYLIST
-#EXT-X-STREAM-INF:PROGRAM-ID=1,NAME=Low,BANDWIDTH=707099,RESOLUTION=1280x720
-#{@upload.low_quality_playlist_url.sub('https:', 'http:')}
-LQ_PLAYLIST
-    end
-
-    playlist =<<-PLAYLIST
-#EXTM3U
-#EXT-X-VERSION:3
-#{hq_playlist}#{lq_playlist}
-PLAYLIST
-
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Accept-Ranges'] = 'bytes'
 
-    render text: playlist, content_type: 'audio/x-mpegurl'
+    render text: @upload.to_m3u8 #, content_type: 'audio/x-mpegurl'
   end
 
   def create
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,9 +3,3 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-Mime::Type.register 'application/x-mpegurl', :m3u8
-
-ActionController::Renderers.add :m3u8 do |object, options|
-  self.content_type ||= 'application/x-mpegurl'
-  object
-end
