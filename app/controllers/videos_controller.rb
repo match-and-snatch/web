a4bb91c9805a0ac20cce290cf67d04e7ef0d4876
Sergei Zinin <zinin@xakep.ru>
2015-02-17 21:37:15 +0700

    Hide resolution, put label in player

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,14 +3,14 @@ class VideosController < UploadsController
   def playlist
     if @upload.high_quality_playlist_url
       hq_playlist = <<-HQ_PLAYLIST
-#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1677946,RESOLUTION=1920x1080
+#EXT-X-STREAM-INF:PROGRAM-ID=1,NAME=High,BANDWIDTH=1677946,RESOLUTION=1920x1080
 #{@upload.high_quality_playlist_url.sub('https:', 'http:')}
 HQ_PLAYLIST
     end
 
     if @upload.low_quality_playlist_url
       lq_playlist = <<-LQ_PLAYLIST
-#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=707099,RESOLUTION=1280x720
+#EXT-X-STREAM-INF:PROGRAM-ID=1,NAME=Low,BANDWIDTH=707099,RESOLUTION=1280x720
 #{@upload.low_quality_playlist_url.sub('https:', 'http:')}
 LQ_PLAYLIST
     end
