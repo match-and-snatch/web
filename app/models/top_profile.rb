12d7aec4cc405f253d287996126290ddc024fc4e
Sergei Zinin <zinin@xakep.ru>
2015-02-28 14:16:05 +0700

    Top profiles collection

---
+++ app/models/top_profile.rb
@@ -0,0 +1,14 @@
+class TopProfile < ActiveRecord::Base
+  belongs_to :user
+  scope :ordered, -> { order(:position) }
+
+  # Rebuilds list of top user profiles
+  # @param user_ids [Array<Ingeter, String>]
+  def self.update_list(user_ids)
+    delete_all
+
+    user_ids.each_with_index do |user_id, index|
+      create!(user_id: user_id, position: index)
+    end
+  end
+end
