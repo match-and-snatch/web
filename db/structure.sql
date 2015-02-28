775e6a354b32b19526751706b4801e5f4e0fae31
Sergei Zinin <zinin@xakep.ru>
2015-02-28 13:56:40 +0700

    Top profiles table

--- db/structure.sql
+++ db/structure.sql
@@ -813,6 +813,36 @@ ALTER SEQUENCE subscriptions_id_seq OWNED BY subscriptions.id;
 
 
 --
+-- Name: top_profiles; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE top_profiles (
+    id integer NOT NULL,
+    user_id integer,
+    "position" integer DEFAULT 0 NOT NULL
+);
+
+
+--
+-- Name: top_profiles_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE top_profiles_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: top_profiles_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE top_profiles_id_seq OWNED BY top_profiles.id;
+
+
+--
 -- Name: uploads; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -1094,6 +1124,13 @@ ALTER TABLE ONLY subscriptions ALTER COLUMN id SET DEFAULT nextval('subscription
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
+ALTER TABLE ONLY top_profiles ALTER COLUMN id SET DEFAULT nextval('top_profiles_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
 ALTER TABLE ONLY uploads ALTER COLUMN id SET DEFAULT nextval('uploads_id_seq'::regclass);
 
 
@@ -1262,6 +1299,14 @@ ALTER TABLE ONLY subscription_daily_count_change_events
 
 ALTER TABLE ONLY subscriptions
     ADD CONSTRAINT subscriptions_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: top_profiles_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY top_profiles
+    ADD CONSTRAINT top_profiles_pkey PRIMARY KEY (id);
 
 
 --
@@ -1529,6 +1574,8 @@ INSERT INTO schema_migrations (version) VALUES ('20150216164150');
 INSERT INTO schema_migrations (version) VALUES ('20150216190226');
 
 INSERT INTO schema_migrations (version) VALUES ('20150217120737');
+
+INSERT INTO schema_migrations (version) VALUES ('20150228065448');
 
 INSERT INTO schema_migrations (version) VALUES ('20150301060034');
 
