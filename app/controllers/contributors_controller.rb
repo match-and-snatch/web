4dc16dc04e5d586141fe30b8e57ed6e07d587c0c
Sergei Zinin <zinin@xakep.ru>
2015-04-03 10:43:23 +0700

    Fix chronic parsing for contributor dates

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -4,7 +4,7 @@ class ContributorsController < ApplicationController
   def index
     @contributions = Contribution.includes(:user).where(target_user_id: current_user.id).order('contributions.created_at DESC')
     if params[:year_month].present?
-      year_month = Chronic.parse(params[:year_month])
+      year_month = YearMonth.to_date_from_string(params[:year_month])
       @contributions = @contributions.where(created_at: (year_month.beginning_of_month..year_month.end_of_month))
     end
     json_popup
