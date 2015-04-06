af46df73df85353abcbae66f9ba29d92fd609e4b
Sergei Zinin <zinin@xakep.ru>
2015-04-06 15:56:35 +0700

    Remove unsubscribed feed events from feed

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,6 +6,7 @@ class FeedsController < ApplicationController
     subscription_user_ids = User.joins(:source_subscriptions).
       where(subscriptions: {user_id: current_user.id}).pluck(:target_user_id)
     @feed_events = FeedEvent.
+      where("type <> 'UnsubscribedFeedEvent'").
       where('target_user_id = ? OR subscription_target_user_id IN (?)', current_user.id, subscription_user_ids).
       order('feed_events.created_at DESC').limit(70).to_a
 
