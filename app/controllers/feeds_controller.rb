50e3ea293c47050632f4eda61115baf4a9c8483d
Sergei Zinin <zinin@xakep.ru>
2015-04-10 14:36:41 +0700

    Show unsubscribers for tough girls

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,10 +5,16 @@ class FeedsController < ApplicationController
   def show
     subscription_user_ids = User.joins(:source_subscriptions).
       where(subscriptions: {user_id: current_user.id}).pluck(:target_user_id)
+
     @feed_events = FeedEvent.
-      where("type <> 'UnsubscribedFeedEvent'").
       where('target_user_id = ? OR subscription_target_user_id IN (?)', current_user.id, subscription_user_ids).
-      order('feed_events.created_at DESC').limit(70).to_a
+      order('feed_events.created_at DESC')
+
+    if current_user.object.registration_token != "GkTB3tmMC3ivokQWMOXdng" # Wendy
+      @feed_events = @feed_events.where("type <> 'UnsubscribedFeedEvent'")
+    end
+
+    @feed_events = @feed_events.order('feed_events.created_at DESC').limit(70).to_a
 
     json_render
   end
