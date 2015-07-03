eb79458a3dfc17166adf6337723a359a99f7a4c0
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2015-07-03 15:41:01 +0700

    Fix pending video response data

--- app/controllers/api/video_posts_controller.rb
+++ app/controllers/api/video_posts_controller.rb
@@ -1,6 +1,6 @@
 class Api::VideoPostsController < Api::MediaPostsController
   def new
-    json_success pending_video_post_data
+    json_success pending_video_post_data(current_user.pending_videos.last)
   end
 
   def cancel
@@ -14,12 +14,14 @@ class Api::VideoPostsController < Api::MediaPostsController
     manager.create_video_post params.slice(:title, :keyword_text, :message, :preview_url).merge(notify: params.bool(:notify))
   end
 
-  def pending_video_post_data
-    custom_preview = current_user.pending_video_previews.last if current_user.pending_video_previews.count > 2
+  def pending_video_post_data(video)
+    custom_preview = if current_user.pending_video_previews.count > 2
+                       current_user.pending_video_previews.last
+                     end
     video_post_data = {
       post_type: 'VideoPost',
       preview_url: (custom_preview ? custom_preview.url : current_user.pending_video_previews.first.try(:url)),
-      video: video_data.tap do |data|
+      video: api_response.pending_video_data(video).tap do |data|
         if custom_preview
           data[:custom_preview] = {
             id: custom_preview.id,
@@ -29,17 +31,5 @@ class Api::VideoPostsController < Api::MediaPostsController
       end
     }
     pending_post_data.merge(video_post_data)
-  end
-
-  def video_data
-    {
-      id: current_user.pending_videos.last.try(:id),
-      previews: current_user.object.pending_video_preview_photos(true).first(2).map do |preview|
-        {
-          id: preview.id,
-          url: preview.url
-        }
-      end
-    }
   end
 end
