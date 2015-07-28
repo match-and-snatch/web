026dc284b2b0cff295ab9be8b557cbfdf7bd0973
Sergei Zinin <zinin@xakep.ru>
2015-07-28 23:43:27 +0700

    Fix price change request jobs

--- app/jobs/costs/change_cost_job.rb
+++ app/jobs/costs/change_cost_job.rb
@@ -1,7 +1,7 @@
 module Costs
   class ChangeCostJob
     def self.perform
-      CostChangeRequest.includes(:user).approved.find_each do |cost_change_request|
+      CostChangeRequest.includes(:user).approved.not_performed.find_each do |cost_change_request|
         UserProfileManager.new(cost_change_request.user).change_cost!(cost: cost_change_request.new_cost,
                                                                       update_existing_subscriptions: cost_change_request.update_existing_subscriptions)
         cost_change_request.perform!
