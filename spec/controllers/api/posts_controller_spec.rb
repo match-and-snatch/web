8ee03fcf7e4b2dcc3983f8c14693fcb7fa689fcb
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2015-08-17 23:12:48 +0700

    Add specs

--- spec/controllers/api/posts_controller_spec.rb
+++ spec/controllers/api/posts_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Api::PostsController, type: :controller do
   let(:poster) { create_profile email: 'poster@gmail.com', api_token: 'test_token' }
+  let(:another_poster) { create_user email: 'anther@poster.ru' }
   let(:_post) { PostManager.new(user: poster).create_status_post(message: 'some post') }
 
   describe 'GET #index' do
@@ -35,9 +36,7 @@ describe Api::PostsController, type: :controller do
   describe 'DELETE #destroy' do
     before { sign_in_with_token(poster.api_token) }
 
-    let(:post) { PostManager.new(user: poster).create_status_post(message: 'test') }
-
-    subject { delete 'destroy', id: post.id }
+    subject { delete 'destroy', id: _post.id }
 
     it { should be_success }
 
@@ -48,8 +47,53 @@ describe Api::PostsController, type: :controller do
     end
 
     context 'unauthorized access' do
-      let(:another_poster) { create_user email: 'anther@poster.ru' }
-      let(:post) { PostManager.new(user: another_poster).create_status_post(message: 'test') }
+      let(:_post) { PostManager.new(user: another_poster).create_status_post(message: 'test') }
+
+      its(:status) { should eq(401) }
+    end
+  end
+
+  describe 'PATCH #update' do
+    before { sign_in_with_token(poster.api_token) }
+    subject(:perform_request) { patch 'update', id: _post.id, title: 'new title', message: 'new message' }
+
+    it { should be_success }
+
+    context 'media post' do
+      let(:_post) do
+        create_audio_upload poster
+        PostManager.new(user: poster).create_audio_post(message: 'test', title: 'test')
+      end
+
+      it { should be_success }
+
+      specify do
+        expect { perform_request }.to change { _post.uploads.count }.to(0)
+      end
+
+      context 'remove upload' do
+        subject(:perform_request) { patch 'update', id: _post.id, title: 'new title', message: 'new message', uploads: [_post.uploads.first.id] }
+
+        it { should be_success }
+
+        specify do
+          expect { perform_request }.to change { _post.uploads.count }.by(-1)
+        end
+      end
+
+      context 'does not remove uploads' do
+        subject(:perform_request) { patch 'update', id: _post.id, title: 'new title', message: 'new message', uploads: _post.uploads.map(&:id) }
+
+        it { should be_success }
+
+        specify do
+          expect { perform_request }.not_to change { _post.uploads.count }
+        end
+      end
+    end
+
+    context 'unauthorized access' do
+      let(:_post) { PostManager.new(user: another_poster).create_status_post(message: 'test') }
 
       its(:status) { should eq(401) }
     end
