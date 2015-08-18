78a7d082efef52408e37d575db261a66c43fcf54
Sergei Zinin <zinin@xakep.ru>
2015-08-18 23:17:07 +0700

    Add memcached to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ BuddyPlatform::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, "memcached.connectpal.com"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   #config.action_controller.asset_host = "//s3-us-west-1.amazonaws.com/buddy-assets"
