ae09ac95f38be5f09997e5ae7565202dc026dbd5
Sergei Zinin <zinin@xakep.ru>
2015-08-18 13:10:13 +0700

    Do not display contribution feed events on my feed

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -18,7 +18,7 @@ class FeedsController < ApplicationController
       @feed_events = @feed_events.where("type <> 'UnsubscribedFeedEvent'")
     end
 
-    @feed_events = @feed_events.order('feed_events.created_at DESC').limit(70).to_a
+    @feed_events = @feed_events.where("feed_events.type <> 'ContributionFeedEvent'").order('feed_events.created_at DESC').limit(70).to_a
 
     json_render
   end
