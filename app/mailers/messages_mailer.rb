86413bc551f64bcdfc51f27d8a5251f316f29db3
Dmitry Jakovlev <dmitry@connectpal.com>
2015-09-02 16:43:09 +0700

    Fix mail about new message

--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -2,10 +2,12 @@ class MessagesMailer < ApplicationMailer
   add_template_helper ApplicationHelper
 
   def new_message(message)
+    @message = message
+
+    return unless @message
     return unless @message.user
     return unless @message.target_user
 
-    @message = message
     mail to: @message.target_user.email, subject: 'New message on ConnectPal'
   end
 end
