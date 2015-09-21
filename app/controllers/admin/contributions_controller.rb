84a34c460461b079a136618aff5265a3f654d393
Sergei <sergei@connectpal.com>
2015-09-22 01:21:03 +0700

    Paginate contributions by month

--- app/controllers/admin/contributions_controller.rb
+++ app/controllers/admin/contributions_controller.rb
@@ -2,7 +2,17 @@ class Admin::ContributionsController < Admin::BaseController
   before_action :load_contribution!, only: [:confirm_destroy, :destroy]
 
   def index
-    @contributions = Contribution.all.includes(:user, :target_user).page(params[:page]).per(100)
+    @months = Contribution.group("date_trunc('month', created_at)").count.to_a.sort_by(&:first)
+
+    @date = Chronic.parse(params[:month])
+    @contributions = Contribution.all.includes(:user, :target_user).page(params[:page])
+
+    if @date
+      @contributions = @contributions.where("contributions.created_at BETWEEN ? AND ? ", @date.beginning_of_month, @date.end_of_month).per(10000)
+    else
+      @contributions.per(100)
+    end
+
     json_render
   end
 
