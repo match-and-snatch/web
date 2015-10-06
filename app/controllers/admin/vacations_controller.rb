d4affe3e84690a4c5641cb4fc27d36b90a4265fc
Sergei <sergei@connectpal.com>
2015-10-06 19:00:14 +0700

    Simplify vacations

--- app/controllers/admin/vacations_controller.rb
+++ app/controllers/admin/vacations_controller.rb
@@ -2,19 +2,16 @@ class Admin::VacationsController < Admin::BaseController
   before_action :load_user!, only: [:history]
 
   def index
-    query = User.profile_owners.
-        joins(:events).
-        where(events: {action: %w[vacation_mode_enabled vacation_mode_disabled]}).
-        where('users.subscribers_count > ?', 0).
-        group('users.id')
+    events = Event.where(action: %w[vacation_mode_enabled vacation_mode_disabled])
+               .joins(:user).where('users.subscribers_count > 0')
+               .preload(:user)
+               .group_by(&:user)
+               .sort_by { |_, events| events.max_by(&:created_at) }
 
-    if params[:sort_by]
-      query = query.order("#{params[:sort_by]} #{params[:sort_direction]}")
-    else
-      query = query.order(subscribers_count: :desc)
+    @vacations = events.map do |user, events|
+      VacationsPresenter.new(user: user, events: events.sort_by(&:created_at))
     end
-    events = Event.where(action: %w[vacation_mode_enabled vacation_mode_disabled], user_id: query.map(&:id)).group_by(&:user_id)
-    @vacations = query.map {|user| VacationsPresenter.new(user: user, events: events[user.id].sort_by(&:created_at))}
+
     json_render
   end
 
