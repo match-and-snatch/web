a27bf6e610fb1c60cc8fe3d30a638330333c8dd2
Sergei <sergei@connectpal.com>
2015-10-22 00:12:34 +0700

    Fix migrations

--- db/migrate/20151021163506_populate_last_time_locked_at.rb
+++ db/migrate/20151021163506_populate_last_time_locked_at.rb
@@ -1,9 +1,9 @@
 class PopulateLastTimeLockedAt < ActiveRecord::Migration
   def up
-    User.where('last_time_locked_at IS NULL').update_all(last_time_locked_at: 3.years.ago)
+    User.where("last_time_locked_at IS NULL AND locked = 't'").update_all(last_time_locked_at: 3.years.ago)
   end
 
   def down
-    User.where('last_time_locked_at < ?', 2.years.ago).update_all(last_time_locked_at: nil)
+    User.where("last_time_locked_at < ? AND locked = 't'", 2.years.ago).update_all(last_time_locked_at: nil)
   end
 end
