03d3470b866f3b2b4f00b7f30373693f0cae52d2
Dmitry Jakovlev <dmitry@connectpal.com>
2015-10-30 15:31:02 +0700

    Fix clean posts job

--- app/jobs/posts/clean_job.rb
+++ app/jobs/posts/clean_job.rb
@@ -1,16 +1,28 @@
 module Posts
   class CleanJob
+    PERIOD = 2.months
+
     def self.perform
       return if Rails.env.staging?
 
-      User.joins(:posts, :events).where(['users.is_profile_owner = ? AND events.action = ? AND events.created_at <= ?', false, 'profile_page_removed', 1.month.ago]).find_each do |user|
-        user.posts.includes(:uploads).find_each do |post|
-          unless post.status?
-            post.uploads.find_each do |upload|
-              upload.delete_s3_files!
+      sql = <<-SQL.squish, false, 'profile_page_removed', PERIOD.ago, 0
+        (users.is_profile_owner = ?
+        AND events.action = ?
+        AND events.created_at <= ?
+        AND subscribers_count = ?)
+      SQL
+
+      User.joins(:posts, :events).where(sql).group('users.id').find_each do |user|
+        event = user.events.where(action: 'profile_page_removed').order(created_at: :desc).first
+        if event && event.created_at <= PERIOD.ago
+          user.posts.includes(:uploads).find_each do |post|
+            unless post.status?
+              post.uploads.find_each do |upload|
+                upload.delete_s3_files!
+              end
             end
+            post.destroy
           end
-          post.destroy
         end
       end
     end
--- spec/jobs/posts/clean_job_spec.rb
+++ spec/jobs/posts/clean_job_spec.rb
@@ -5,30 +5,49 @@ describe Posts::CleanJob do
     subject(:perform) { described_class.perform }
 
     let(:user) { create_profile }
+    let(:manager) { UserProfileManager.new(user) }
 
     before do
       UploadManager.new(user).create_pending_photos(transloadit_photo_data_params)
       PostManager.new(user: user).create_photo_post(title: 'test', message: 'test')
     end
 
-    specify { expect { perform }.not_to raise_error }
+    it { expect { perform }.not_to raise_error }
 
-    context '30 days not passed after remove profile' do
-      before { UserProfileManager.new(user).delete_profile_page! }
+    context '2 months not passed after remove profile' do
+      before { manager.delete_profile_page! }
 
-      it { expect { perform }.not_to change { Post.count } }
+      it { expect { perform }.not_to delete_record(Post) }
     end
 
-    context '30 days passed since profile removed' do
+    context '2 months passed since profile removed' do
       before do
-        Timecop.freeze(1.month.ago) do
-          UserProfileManager.new(user).delete_profile_page!
+        Timecop.freeze(2.month.ago) do
+          manager.delete_profile_page!
           Upload.delete_all
         end
       end
 
+      it { expect { perform rescue nil }.to delete_record(Post) }
+    end
+
+    context '2 months ago user removed and restored his profile and removed profile yesterday' do
+      before do
+        Timecop.freeze(2.month.ago) do
+          manager.delete_profile_page!
+          Upload.delete_all
+          manager.create_profile_page
+        end
+        Timecop.freeze(1.day.ago) do
+          manager.delete_profile_page!
+        end
+      end
+
+      it { expect { perform }.not_to delete_record(Post) }
       it do
-        expect { perform rescue nil }.to change { Post.count }.from(1).to(0)
+        Timecop.freeze(2.month.from_now) do
+          expect { perform }.to delete_record(Post)
+        end
       end
     end
   end
