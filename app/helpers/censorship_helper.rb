8d879cc7fd15c44a4a9dc558af0e7df280b0bdef
Dmitry Jakovlev <dmitry@connectpal.com>
2015-12-02 16:40:34 +0700

    Remove all adult words from stripe and emails

--- app/helpers/censorship_helper.rb
+++ app/helpers/censorship_helper.rb
@@ -1,5 +1,5 @@
 module CensorshipHelper
   def cut_adult_words(sentence = nil)
-    sentence.to_s.gsub(/xxx/i, '').strip
+    sentence.to_s.gsub(/(xxx|\bporn\b|\bsex\b|\btits\b|\bboobs\b|\bass\b|\bpussy\b|\bcum\b|\b69\b)/i, '').squish
   end
 end
