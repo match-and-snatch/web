6de2b1cbe49cc40f4c848a7af1d4aa43e1085d2a
Dmitry Jakovlev <dmitry@connectpal.com>
2015-12-03 11:32:21 +0700

    Move adult filter regex to config

--- app/managers/payment_manager.rb
+++ app/managers/payment_manager.rb
@@ -109,6 +109,6 @@ class PaymentManager < BaseManager
   end
 
   def cut_adult_words(sentence = nil)
-    sentence.to_s.gsub(/(xxx|\bporn\b|\bsex\b|\btits\b|\bboobs\b|\bass\b|\bpussy\b|\bcum\b|\b69\b)/i, '').squish
+    sentence.to_s.gsub(APP_CONFIG['adult_filter_regex'], '').squish
   end
 end
