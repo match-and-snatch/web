05c2bb99253b175cdb88a42ec6ef86b8bd12b50c
Sergei <sergei@connectpal.com>
2016-02-01 13:33:36 +0700

    Fix year validation

--- app/managers/concerns/credit_card_validator.rb
+++ app/managers/concerns/credit_card_validator.rb
@@ -8,7 +8,7 @@ module Concerns::CreditCardValidator
       fail_with :cvc if card.cvc.blank? || card.cvc.length < 3
     end
 
-    fail_with :expiry_date if card.expiry_month.blank? || card.expiry_year.blank? || card.expiry_month.to_i > 12 || card.expiry_month.to_i < 1 || card.expiry_year.to_i < 14
+    fail_with :expiry_date if card.expiry_month.blank? || card.expiry_year.blank? || card.expiry_month.to_i > 12 || card.expiry_month.to_i < 1 || card.expiry_year.to_i < 15
     fail_with zip: :empty if card.zip.blank?
     fail_with city: :empty if card.city.blank?
     fail_with state: :empty if card.state.blank?
