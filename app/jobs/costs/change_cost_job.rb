f17a3a55d6212972ce7e6db5b9afbcdd8dfb739e
Dmitry Jakovlev <dmitry@connectpal.com>
2016-02-12 21:12:17 +0700

    Fix cost change job report

--- app/jobs/costs/change_cost_job.rb
+++ app/jobs/costs/change_cost_job.rb
@@ -3,13 +3,14 @@ module Costs
     include Concerns::Jobs::Reportable
 
     def perform
-      report = new_report requests_to_processing: requests.count, failed_requests: 0
+      report = new_report requests_to_processing: requests.count, performed_requests: 0, failed_requests: 0
 
       requests.find_each do |cost_change_request|
         begin
           UserProfileManager.new(cost_change_request.user)
               .change_cost!(cost: cost_change_request.new_cost, update_existing_subscriptions: cost_change_request.update_existing_subscriptions)
           cost_change_request.perform!
+          report[:performed_requests] += 1
         rescue ManagerError => e
           report.log_failure(e.message)
           report[:failed_requests] += 1
