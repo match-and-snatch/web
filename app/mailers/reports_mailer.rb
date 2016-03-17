6a4e0820942b1e195ca514ef35047c9475264fd4
Dmitry Jakovlev <dmitry@connectpal.com>
2016-03-17 20:19:28 +0700

    Add YARD to reports mailer

--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -1,11 +1,13 @@
 class ReportsMailer < ApplicationMailer
   default to: APP_CONFIG['emails']['reports']
 
+  # @param report [Report]
   def job_report(report)
     @report = report
     mail subject: @report.title
   end
 
+  # @param report [User]
   def deleted_posts_too_often(user)
     @user = user
     mail subject: "[#{user.id} - #{user.email}] deleted 5 or more posts in a hour"
