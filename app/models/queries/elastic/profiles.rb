3b65e2630f06189d68545b4486f1c5d8b63a43b3
Dmitry Jakovlev <dmitry@connectpal.com>
2016-03-22 16:12:17 +0700

    Enable search by hidden profiles

--- app/models/queries/elastic/profiles.rb
+++ app/models/queries/elastic/profiles.rb
@@ -6,37 +6,33 @@ module Queries
       scope do
         User.profile_owners
             .with_complete_profile
-            .where(hidden: false, has_mature_content: false)
-            .where('users.subscribers_count > 0 OR users.profile_picture_url IS NOT NULL')
       end
 
-      body do |fulltext_query|
-        {
+      body do |fulltext_query, include_hidden: false|
+        query = {
           query: {
-            filtered: {
+            function_score: {
               query: {
-                function_score: {
-                  query: {
-                    dis_max: {
-                      queries: [
-                        {match: {profile_name: fulltext_query}},
-                        {match: {full_name: fulltext_query}},
-                        {match: {profile_types_text: fulltext_query}}
-                      ]
-                    }
-                  },
-                  field_value_factor: {
-                    field: 'subscribers_count',
-                    modifier: 'log1p',
-                    factor: 0.15
-                  },
-                  boost_mode: 'sum'
+                dis_max: {
+                  queries: [
+                    {match: {profile_name: fulltext_query}},
+                    {match: {full_name: fulltext_query}},
+                    {match: {profile_types_text: fulltext_query}}
+                  ]
                 }
               },
-              filter: {term: {publicly_visible: true}}
+              field_value_factor: {
+                field: 'subscribers_count',
+                modifier: 'log1p',
+                factor: 0.15
+              },
+              boost_mode: 'sum'
             }
           }
         }
+        query[:filter] = {term: {publicly_visible: true}} unless include_hidden
+
+        {query: {filtered: query}}
       end
     end
   end
