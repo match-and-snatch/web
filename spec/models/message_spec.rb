fb0bf5eeae36c3315ed3902d155e2daf1b311424
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2016-04-25 16:26:05 +0700

    Handle links in messages (#498)

---
+++ spec/models/message_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Message do
+  let(:user) { create(:user) }
+  let(:target_user) { create(:user) }
+  let(:message) { create :message, user: user, target_user: target_user }
+end
