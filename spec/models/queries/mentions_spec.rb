9849803cc58d27bf2c4be66c9a42e721b039c3b7
Dmitry Jakovlev <dmitry@connectpal.com>
2016-05-03 22:19:39 +0700

    Fix specs

--- spec/models/queries/mentions_spec.rb
+++ spec/models/queries/mentions_spec.rb
@@ -8,6 +8,7 @@ describe Queries::Mentions do
   before do
     SubscriptionManager.new(subscriber: subscriber).subscribe_to(profile_owner)
     SubscriptionManager.new(subscriber: current_user).subscribe_to(profile_owner)
+    update_index
   end
 
   subject(:query) { described_class.new(query: q, current_user: current_user, profile_id: profile_owner.id)  }
@@ -53,6 +54,18 @@ describe Queries::Mentions do
       context 'querying self' do
         let(:q) { 'Dmitry' }
         it { is_expected.to eq([current_user]) }
+      end
+
+      context 'querying after subscription' do
+        let(:another_user) { create :user, :with_cc, full_name: 'Donald' }
+
+        before do
+          SubscriptionManager.new(subscriber: another_user).subscribe_to(profile_owner)
+          sleep 1
+        end
+
+        let(:q) { 'Donald' }
+        it { is_expected.to eq([another_user]) }
       end
     end
   end
